3806bac929b8342ee5e842eb25c0e60789f3a982
surekha <natu.sulekha87@gmail.com>
2024-08-19 18:10:24 +0100

    Added captcha Google verification

--- app/Livewire/SupportRequest.php
+++ app/Livewire/SupportRequest.php
@@ -9,7 +9,7 @@ use Filament\Forms\Components\MarkdownEditor;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
 use Filament\Forms\Form;
-use Mews\Captcha\Captcha;
+use AbanoubNassem\FilamentGRecaptchaField\Forms\Components\GRecaptcha;
 use Livewire\Component;
 
 class SupportRequest extends Component implements HasForms
@@ -38,20 +38,25 @@ class SupportRequest extends Component implements HasForms
                     ->minLength(6)
                     ->maxLength(100)
                     ->rows(5),
+                GRecaptcha::make('captcha')
             ])
             ->statePath('data');
     }
     
+    /**
+     * Store request
+     *
+     * @return void
+     */
     public function submitRequest(): void
     {
         try {
             Support::create($this->form->getState());
             session()->flash('success', 'Thank you. Your request has been sent.');
+            $this->resetForm();
         } catch (\Exception $ex) {
             session()->flash('error', 'Something goes wrong!!');
         }
-
-        $this->resetForm();
     }
 
     private function resetForm()
--- resources/views/livewire/support-request.blade.php
+++ resources/views/livewire/support-request.blade.php
@@ -2,23 +2,18 @@
     <form wire:submit="submitRequest">
         @csrf
 
-        <div id="message">
+        <div class="message">
             @if (session()->has('success'))   
                 <div class="flex alert-success" role="alert">
                     {{ session('success') }}
                 </div>
-
-                <script>
-                    var elmnt = document.getElementById("message");
-                    elmnt.scrollIntoView();
-                </script>
             @endif
         </div>
 
-        <div>
+        <div class="message">
             @if (session()->has('error'))   
                 <div class="flex alert-danger" role="alert">
-                    <span class="font-medium">Error! </span>{{ session('error') }}
+                    <span class="font-medium">Error! &nbsp;</span>{{ session('error') }}
                 </div>
             @endif
         </div>
@@ -28,9 +23,17 @@
         </div>
         
         <div class="flex justify-end mt-5">
-            <button type="submit" class="button-primary">
+            <button type="submit" class="button-primary" x-on:click="scrollToTop">
                 Submit
             </button>
         </div>
     </form>
 </div>
+
+<script>
+    function scrollToTop() {
+        setTimeout(() => {
+            window.scrollTo({top: 0, behavior: 'smooth'});
+        }, 1000);
+    }
+</script>
